e28e37a14b110e4bb87c16f1ee10570722190c36
Scott <smclendening@gmail.com>
2017-09-18 16:30:14 -0700

    basic app

---
+++ client/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+//import configureStore from './configureStore';
+//import App from './components/App.jsx';
+
+//const store = configureStore();
+
+const Root = () => (
+   // <Provider store={store}>
+   //   <App />
+  //  </Provider>
+  <div>Hello World</div>
+)
+
+ReactDOM.render(<Root />, document.getElementById('app'));
